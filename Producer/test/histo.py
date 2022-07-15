575029d5f81ce70550e0674c4772f1e13d5562b4
Gabriel Soto <gabriel.jerardo.soto@cern.ch>
2022-07-15 23:16:40 +0200

    editted histo.py and updated electron pt

--- Producer/test/histo.py
+++ Producer/test/histo.py
@@ -3,15 +3,30 @@ import numpy as np
 
 f1 = ROOT.TFile("output.root", "OPEN")
 
-#-----------ELECTRON_PT-------------#
-NBin = 10
-binning =  np.array((0,15,25,45,55,70,80,95,130,190))
-h1 = ROOT.TH1D("h1",";pT [GeV];410/15 ",NBin,binning)
+NBin = 13
+binning =  np.array((0.,15.,30.,45.,60.,75.,90.,105.,120.,135.,150.,165.,180.,195.))
 
-for evt1 in f1.Get("egammaReconstruction/tree"):
+h1 = ROOT.TH1F("h1",";pT [GeV];410/15 ",NBin,binning)
+h2 =ROOT.TH1F("h2","Electron phi",10,-4,4)
+h3 = ROOT.TH1F("h3","Electron eta",10,-4,4)
+
+
+for evt in f1.Get("egammaReconstruction/tree"):
 	
-	for i in range(0,len(evt1.electron_pt)):
-		h1.Fill(evt1.electron_pt[i])
+#-----------------ELECTRON_PT--------------------#
+	for i in range(0,len(evt.electron_pt)):
+		h1.Fill(evt.electron_pt[i])
+
+#-----------------ELECTRON_PHI-------------------#
+
+	for j in range(0,len(evt.electron_phi)):
+		h2.Fill(evt.electron_phi[j])
+
+#-----------------ELECTRON_ETA-------------------#
+
+	for k in range(0,len(evt.electron_eta)):
+		h3.Fill(evt.electron_eta[k])
+
 
 c1 = ROOT.TCanvas("c1","c1",1000,1000)
 
@@ -24,15 +39,6 @@ c1.Draw()
 
 c1.SaveAs("Electron_pt_var_bin.png")
 
-#-----------ELECTRON_PHI-------------#
-
-h2 =ROOT.TH1F("h2","Electron phi",10,-4,4)
-
-for evt2 in f1.Get("egammaReconstruction/tree"):
-
-        for j in range(0,len(evt2.electron_phi)):
-                h2.Fill(evt2.electron_phi[j])
-
 c2 = ROOT.TCanvas("c2","c2",1000,1000)
 
 h2.SetTitle("Electron #phi")
@@ -43,15 +49,6 @@ h2.Draw("C9E1 SAME")
 c2.Draw()
 
 c2.SaveAs("Electron_phi.png")
-
-#-----------ELECTRON_PT-------------#
-
-h3 = ROOT.TH1F("h3","Electron eta",10,-4,4)
-
-for evt3 in f1.Get("egammaReconstruction/tree"):
-
-        for k in range(0,len(evt3.electron_eta)):
-                h3.Fill(evt3.electron_eta[k])
 
 c3 =ROOT.TCanvas("c3","c3",1000,1000)
 
