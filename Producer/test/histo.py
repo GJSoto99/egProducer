0cf0da718e776a881eeb332082da019149e2ac7c
Gabriel Soto <gabriel.jerardo.soto@cern.ch>
2022-07-24 22:17:19 +0200

    adding output

--- Producer/test/histo.py
+++ Producer/test/histo.py
@@ -1,5 +1,6 @@
 import ROOT
 import numpy as np
+import vector as vec
 
 f1 = ROOT.TFile("output.root", "OPEN")
 
@@ -9,13 +10,13 @@ binning =  np.array((0.,15.,30.,45.,60.,75.,90.,105.,120.,135.,150.,165.,180.,19
 h1 = ROOT.TH1F("h1",";pT [GeV];410/15 ",NBin,binning)
 h2 = ROOT.TH1F("h2","Electron phi",10,-4,4)
 h3 = ROOT.TH1F("h3","Electron eta",10,-4,4)
-h4 = ROOT.TH1F("h4","Di-Electron invariant mass",20,0,100)
-
-nElectron = range(0, len(evt.electron_mass))
+h4 = ROOT.TH1F("h4","Di-Electron invariant mass",10,0,0.01)
+h5 = ROOT.TH1F("h5","Di-Electron invariant mass",10,0,0.01)
 
 for evt in f1.Get("egammaReconstruction/tree"):
 	
 #------------------ELECTRON_PT--------------------#
+	
 	for i in range(0,len(evt.electron_pt)):
 		h1.Fill(evt.electron_pt[i])
 
@@ -30,19 +31,30 @@ for evt in f1.Get("egammaReconstruction/tree"):
 		h3.Fill(evt.electron_eta[k])
 
 #-----------Di-Electron Invariant Mass------------#
-	
-	for e in nElectron:
-	  if nElectron == 2:
+		
+	for e in range(0, len(evt.electron_mass)):
+
+	  if len(evt.electron_mass) != 2:
 		continue
 
-	  if electron_charge[0] != electron_charge[1]:
-		continue
+	  if evt.electron_charge[0] == evt.electron_charge[1]:
+		#continue
 
-	  if electron_mass[1] + electron_mass[0] == 1.022:
-		h4.Fill(evt.electron_mass)
+	  #if (evt.electron_mass[1] + evt.electron_mass[0] < 0.0020) and (evt.electron_mass[1] + evt.electron_mass[0] > 0.0005):
+		h4.Fill(evt.electron_mass[e])
 		
 #------------------------------------------------#
 
+v1 = vec.obj(electron_pt,electron_phi,electron_eta,electron_mass)
+v2 = vec.obj(electron_pt,electron_phi,electron_eta,electron_mass)
+
+#for ent in f1.Get("egammaReconstruction/tree"): 
+
+    #for a in range(0, len(ent.electron_mass)): 
+    
+	  #m = v1.electron_mass[a] + v2.electron_mass[a]
+
+#------------------------------------------------#
 
 c1 = ROOT.TCanvas("c1","c1",1000,1000)
 
@@ -86,6 +98,6 @@ h4.Draw("HIST")
 h4.Draw("C9E1 SAME")
 c4.Draw()
 
-c4.SaveAs("Electron_Events_2000.png")
+c4.SaveAs("Electron_Di-Electron_2000.png")
 
 f1.Close()
